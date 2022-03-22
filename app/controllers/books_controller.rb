6d16e7b949e46050b615dc3aa39ee17035fdf4cd
pavemen3 <43461016+pavemen3@users.noreply.github.com>
2022-03-22 09:33:56 +0000

    ログイン後プロフィール編集できるところまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,16 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
+    @user = User.find(current_user.id)
   end
 
   def create
+    @user = User.find(current_user.id)
     @book = Book.new(book_params)
+    @book.user_id = @user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
@@ -40,6 +44,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
