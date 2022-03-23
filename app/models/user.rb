04500765f9b2bbb57cc9f2654ac8b53fd418b176
pavemen3 <43461016+pavemen3@users.noreply.github.com>
2022-03-23 14:59:58 +0000

    フォロー/フォロワー機能 model controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :follower_id, class_name: "Relationship", foreign_key: "user_id", dependent: :destroy # フォロー取得
+  has_many :followed_id, class_name: "Relationship", foreign_key: "user_id", dependent: :destroy # フォロワー取得
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  has_many :followed_user, through: :followed, source: :follower  # 自分をフォローしている人
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -14,5 +19,20 @@ class User < ApplicationRecord
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  # ユーザーをフォローする、controllerで使用します。
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す、controllerで使用します。
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す、viewで使用します。
+  def following?(user)
+    following_user.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resources :relationships, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
