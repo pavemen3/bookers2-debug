99b45b32329e2af96f52a7034b6795afe5044712
pavemen3 <43461016+pavemen3@users.noreply.github.com>
2022-03-24 12:49:24 +0000

    [finish] 課題6【課題3で実装した機能の非同期通信化】

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,27 +2,15 @@ class BookCommentsController < ApplicationController
   before_action :ensure_correct_user, only: [:destroy]
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
-    # エラーメッセージ表示機能 render時にcommentのパスが生成されてしまう
-    # if comment.save
-    #   redirect_back(fallback_location: root_path)
-    # else
-    #   @error_comment = comment
-    #   @book = Book.find(params[:book_id])
-    #   @user = User.find(@book.user_id)
-    #   @new_book = Book.new
-    #   @book_comment = BookComment.new
-    #   render "books/show"
-    # end
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find(params[:id]).destroy
   end
 
   private
@@ -32,8 +20,8 @@ class BookCommentsController < ApplicationController
   end
 
   def ensure_correct_user
-    @book = Book.find(params[:book_id])
-    @user = User.find(@book.user_id)
+    @comment = BookComment.find(params[:id])
+    @user = User.find(@comment.user_id)
     unless @user == current_user
       redirect_to user_path(current_user)
     end
