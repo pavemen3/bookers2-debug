06a76d3e9a98934424ab36fe1c22c9eadc27953d
pavemen3 <43461016+pavemen3@users.noreply.github.com>
2022-03-28 05:14:50 +0000

    [finish] 課題 7b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,14 @@ class UsersController < ApplicationController
     # @books = @user.books
     @books = @user.books.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
     @book = Book.new
+
+    @today_books = @user.books.created_today
+    @yesterday_books = @user.books.created_yesterday
+    @this_week_books = @user.books.created_this_week
+    @last_week_books = @user.books.created_last_week
+    # 前日比・前週比
+    @the_day_before = ratio_post(@today_books, @yesterday_books)
+    @compared_to_last_week = ratio_post(@this_week_books, @last_week_books)
   end
 
   def index
@@ -37,5 +45,9 @@ class UsersController < ApplicationController
     unless @user == current_user
       redirect_to user_path(current_user)
     end
+  end
+
+  def ratio_post(books, prev_books)
+    prev_books.count > 0 ? (books.count.to_f / prev_books.count.to_f) * 100 : 0.to_f
   end
 end
